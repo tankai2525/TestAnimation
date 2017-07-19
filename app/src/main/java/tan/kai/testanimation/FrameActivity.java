d0f93357d9818ca82cb16ad864d9784b078730c2
luobodidi <1067629251@qq.com>
2017-07-19 18:11:20 +0800

    1 加入帧动画和补间动画<rotate><alpha>例子;

--- app/src/main/java/tan/kai/testanimation/FrameActivity.java
+++ app/src/main/java/tan/kai/testanimation/FrameActivity.java
@@ -1,31 +1,71 @@
 package tan.kai.testanimation;
 
 import android.graphics.drawable.AnimationDrawable;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
 
 /**
- *  帧动画
+ * 帧动画
  */
 public class FrameActivity extends AppCompatActivity {
+
+    private AnimationDrawable frameAnimation;
+    private ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_frame);
 
-        ImageView imageView = (ImageView) findViewById(R.id.img);
-        imageView.setBackgroundResource(R.drawable.frame_animation);
-        final AnimationDrawable frameAnimation = (AnimationDrawable) imageView.getBackground();
-        imageView.post(new Runnable() {
+        imageView = (ImageView) findViewById(R.id.img);
+
+        initXml();//xml方式
+//        initCode();//代码方式
+
+        //启动,第一种方式
+        imageView.post(new Runnable() {//兼容低版本2.3.3
             @Override
             public void run() {
-                //兼容低版本
                 frameAnimation.start();
             }
         });
-
     }
 
+    private void initXml() {
+        //设置imageView的背景为帧动画资源文件
+        imageView.setBackgroundResource(R.drawable.frame_animation);
+        //拿到帧动画背景强转成AnimationDrawable
+        frameAnimation = (AnimationDrawable) imageView.getBackground();
+    }
+
+    private void initCode() {
+        frameAnimation = new AnimationDrawable();
+        for (int i = 1; i < 6; i++) {
+            //拿到资源图片id
+            int id = getResources().getIdentifier("loading_newpic" + i, "mipmap", getPackageName());
+            //根据id拿到Drawable
+            Drawable drawable = ContextCompat.getDrawable(this, id);
+            //添加一帧到帧动画
+            frameAnimation.addFrame(drawable, 200);
+        }
+        //设置循环播放
+        frameAnimation.setOneShot(false);
+        if (Build.VERSION.SDK_INT >= 16) {
+            imageView.setBackground(frameAnimation);
+        } else {
+            imageView.setBackgroundDrawable(frameAnimation);
+        }
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        //启动,第二种方式
+        //开启动画，默认循环播放
+//        frameAnimation.start();
+    }
 }
--- app/src/main/java/tan/kai/testanimation/TweenActivity.java
+++ app/src/main/java/tan/kai/testanimation/TweenActivity.java
@@ -1,31 +1,50 @@
 package tan.kai.testanimation;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 /**
  * 补间动画
+ * 1 alpha 渐变透明
+ * 2 scale 缩放
+ * 3 translate 位移
+ * 4 rotate 旋转
  * Created by tankai on 2017/6/10.
  */
 
 public class TweenActivity extends AppCompatActivity {
 
+    private ImageView imageView;
+    private Context mContext;
+    private Animation animation;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tween);
+        mContext = this;
+        imageView = (ImageView)findViewById(R.id.img);
     }
 
     public void onClick(View view) {
         switch (view.getId()) {
-            case R.id.btn_tween:
+            case R.id.btn_alpha:
+                animation = AnimationUtils.loadAnimation(mContext, R.anim.tween_alpha_anim);
                 break;
-            case R.id.btn_frame:
+            case R.id.btn_scale:
                 break;
-            case R.id.btn_property:
+            case R.id.btn_translate:
+                break;
+            case R.id.btn_rotate:
+                animation = AnimationUtils.loadAnimation(mContext, R.anim.tween_rotate_anim);
                 break;
         }
+        imageView.startAnimation(animation);
     }
 
 }
